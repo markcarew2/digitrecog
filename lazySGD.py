Fixed gradient descent, tried different NN shapes, improved results

lazySGD.py
@@ -2,25 +2,31 @@ from costFunction import costFunction, costy, grady, findDeltas, cFunction
 import random
 import numpy as np
 
-def lazyStochasticGradientDescent(thetas, thetaShapes,X, y, lam,  alpha, iterations, batchSize):
+def shuffle_in_unison(a, b):
+    rng_state = np.random.get_state()
+    np.random.shuffle(a)
+    np.random.set_state(rng_state)
+    np.random.shuffle(b)
+
+def miniBatchGradientDescent(thetas, thetaShapes,X, y, lam,  alpha, iterations, batchSize):
     m = y[:,0].size
-    if m > batchSize:
-        randIndList = random.sample(range(0,m-batchSize),iterations)
-        for i, index in enumerate(randIndList):
-            Xstoch = X[index:index+batchSize,:]
-            ystoch = y[index:index+batchSize]
+    for i in range(iterations):
+        shuffle_in_unison(X,y)
+        if m > batchSize:
+            passes = m // batchSize
+            for j in range(passes):
+                Xstoch = X[j*batchSize:(j+1)*batchSize,:]
+                ystoch = y[j*batchSize:(j+1)*batchSize]
 
-            #DS = findDeltas(thetas,thetaShapes,Xstoch,ystoch, lam)
-            #J = cFunction(thetas,thetaShapes,Xstoch,ystoch, lam)
+                J, DS = costFunction(thetas,thetaShapes,Xstoch,ystoch, lam)
+                thetas = thetas - np.multiply(alpha,DS)
 
-            J, DS = costFunction(thetas,thetaShapes,Xstoch,ystoch, lam)
-            print("The cost at iteration %s is: " % i, J)
-            thetas = thetas - np.multiply(alpha,DS)
-
-    else:
-        randIndList = random.sample(range(0,m-batchSize),iterations)
-        for i, index in enumerate(randIndList):
-            J, DS = costFunction(thetas,thetaShapes,X,y, lam)
-            thetas = thetas - np.multiply(alpha,DS)
-            print("The cost at iteration %s is: " % i,J)
+        else:
+            randIndList = random.sample(range(0,m-batchSize),iterations)
+            for i, index in enumerate(randIndList):
+                J, DS = costFunction(thetas,thetaShapes,X,y, lam)
+                thetas = thetas - np.multiply(alpha,DS)
+        
+        print("The cost at iteration %s is: " % i, J)
+        
     return thetas

main.py
@@ -6,7 +6,7 @@ import random
 from keras.utils import to_categorical
 from scipy.optimize import minimize
 from costFunction import cFunction,findDeltas
-from lazySGD import lazyStochasticGradientDescent
+from lazySGD import miniBatchGradientDescent
 from sigmoid import sigmoid
 from sigmoidDer import siggy
 from accuracy import accuracy, predict
@@ -29,7 +29,6 @@ validX = trainX[50000:, :]
 validX = normalize(validX)
 trainX = trainX[0:50000,:]
 trainX = normalize(trainX)
-print(trainX)
 testX = normalize(testX)
 
 validY = trainY[50000:]
@@ -41,11 +40,11 @@ validY = to_categorical(validY)
 
 #initialize theta matrices
 epsilon = .1
-theta1 = np.random.rand(25,28*28)
+theta1 = np.random.rand(50,28*28)
 theta1 = epsilon * (2*theta1 - 1)
-theta1 = np.concatenate((np.transpose([[1]*25]), theta1), axis=1)
+theta1 = np.concatenate((np.transpose([[1]*50]), theta1), axis=1)
 
-theta2 = np.random.rand(10,25)
+theta2 = np.random.rand(10,50)
 theta2 = epsilon * (2*theta2 - 1)
 theta2 = np.concatenate((np.transpose([[1]*10]), theta2), axis=1)
 
@@ -53,12 +52,11 @@ thetas = np.concatenate((theta1.flatten(), theta2.flatten()))
 
 #Training the network
 startTime = time.time()
-#Best Parameters I've found are batch size = 10000, iterations = 3000, lambda = 1, alpha = .999
-trainedNetwork = lazyStochasticGradientDescent(thetas, [(25,785), (10, 26)], trainX,trainY,0, .999, 15000,1000)
+trainedNetwork = miniBatchGradientDescent(thetas, [(50,785), (10, 51)], trainX,trainY,.00001, .9, 20,100)
 SGDTime = time.time()-startTime
 
-obj_fun = lambda x: cFunction(x, [(25,785), (10, 26)], trainX,trainY,.001)
-gradd = lambda x: findDeltas(x, [(25,785), (10, 26)], trainX,trainY,.001)
+#obj_fun = lambda x: cFunction(x, [(25,785), (10, 26)], trainX,trainY,.001)
+#gradd = lambda x: findDeltas(x, [(25,785), (10, 26)], trainX,trainY,.001)
 
 startTime = time.time()
 #Very slow, might be because of my functions but I think it's just because it uses the full batch
@@ -68,10 +66,10 @@ scpyTime = time.time()-startTime
 print("SGD Took: %s seconds" % SGDTime)
 print("SciPy Took: %s seconds" % scpyTime)
 print("***********************")
-print("Gradient Descent Training Accuracy: ", accuracy(trainX,trainY,[(25,785), (10, 26)],trainedNetwork))
+print("Gradient Descent Training Accuracy: ", accuracy(trainX,trainY,[(50,785), (10, 51)],trainedNetwork))
 #print("SciPy Training Accuracy: ", accuracy(trainX,trainY,[(25,785), (10, 26)],rez.x))
 print("***********************")
-print("Gradient Descent Validation Accuracy: ", accuracy(validX,validY,[(25,785), (10, 26)],trainedNetwork))
+print("Gradient Descent Validation Accuracy: ", accuracy(validX,validY,[(50,785), (10, 51)],trainedNetwork))
 #print("SciPy Validation Accuracy: ", accuracy(validX,validY,[(25,785), (10, 26)],rez.x))
 
 with open('SGDThetas.npy', 'wb') as f: